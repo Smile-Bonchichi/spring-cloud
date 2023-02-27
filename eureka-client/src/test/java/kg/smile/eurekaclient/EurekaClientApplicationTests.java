1c25fed78c818e93cdd75e656ec8256741ab304e
Nurdin <@n.ulanuulu>
2023-02-27 18:02:52 +0600

    add eureka client

---
+++ eureka-client/src/test/java/kg/smile/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.smile.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

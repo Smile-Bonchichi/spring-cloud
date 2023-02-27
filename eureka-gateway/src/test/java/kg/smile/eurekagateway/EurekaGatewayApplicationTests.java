be4509e0083c22f24ef3697d0e43573255e2814d
Nurdin <@n.ulanuulu>
2023-02-27 18:20:31 +0600

    add eureka gateway

---
+++ eureka-gateway/src/test/java/kg/smile/eurekagateway/EurekaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.smile.eurekagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
